a14291ffbe3db27f58986597fabec91151fcc71f
Oleksandr Holyshevskyi <oleksandr.holyshevskyi@pcmicorp.com>
2022-09-07 16:25:21 +0300

    add executor for assertion and executor stuff

---
+++ src/assertion/assertion-executing/assertion-stuff.ts
@@ -0,0 +1,15 @@
+export class AssertionStuff {
+    private succeeded: Boolean;
+
+    public checkCondition(condition: any): AssertionStuff {
+        this.succeeded = new Boolean(condition);
+
+        return this;
+    }
+
+    public throwWithMessage(message: string) {
+        if (!this.succeeded.valueOf()) {
+            throw Error(message);
+        }
+    }
+}
---
+++ src/assertion/assertion-executing/execute.ts
@@ -0,0 +1,5 @@
+import { AssertionStuff } from './assertion-stuff';
+
+export class Execute {
+    public static readonly stuff = new AssertionStuff();
+}
